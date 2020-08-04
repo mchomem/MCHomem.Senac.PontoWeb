87ae2be99a7a10d08c6fa464609ed76a76f397f4
Misael Homem <misael.homem@gmail.com>
2020-08-03 21:44:32 -0300

    Backend updated with Swagger.

--- MCHomem.Senac.PontoWeb.WebAPI/Startup.cs
+++ MCHomem.Senac.PontoWeb.WebAPI/Startup.cs
@@ -49,6 +49,9 @@ namespace MCHomem.Senac.PontoWeb.WebAPI
                 (
                     option => option.JsonSerializerOptions.PropertyNamingPolicy = null
                 );
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +61,16 @@ namespace MCHomem.Senac.PontoWeb.WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Senac evaluation test - API V1");
+            });
 
             app.UseRouting();
             app.UseCors();
